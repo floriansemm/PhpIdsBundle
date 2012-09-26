573286dd86fafd19d58948aedca8f0ee2740b9f6
Florian Semm <florian.semm@gmx.de>
2012-09-26 07:37:49 +0200

    replace services.yml with services.xml

--- DependencyInjection/FSPhpIdsExtension.php
+++ DependencyInjection/FSPhpIdsExtension.php
@@ -2,10 +2,10 @@
 
 namespace FS\PhpIdsBundle\DependencyInjection;
 
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use FS\PhpIdsBundle\DependencyInjection\Configuration\Configuration;
 use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\Config\FileLocator;
 
@@ -15,8 +15,8 @@ use Symfony\Component\Config\FileLocator;
  */
 class FSPhpIdsExtension extends Extension {
     public function load(array $configs, ContainerBuilder $container) {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
         
         $processor     = new Processor();
         $configuration = new Configuration();
